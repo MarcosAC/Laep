1eada933a234d563de2a6dabca8287d86c1a9f4a
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-11-04 16:17:50 -0300

    Botao voltar titleview pagina Projeto Eletrico

--- Laep/Laep/ViewModels/ProjetoEletricoViewModel.cs
+++ Laep/Laep/ViewModels/ProjetoEletricoViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace Laep.ViewModels
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Laep.ViewModels
 {
     public class ProjetoEletricoViewModel : CadastroViewModel
     {
-        
+        private Command _botaoVoltarTitleViewCommand;
+        public Command BotaoVoltarTitleViewCommand =>
+            _botaoVoltarTitleViewCommand ?? (_botaoVoltarTitleViewCommand = new Command(async () => await ExecuteBotaoVoltarTitleViewCommand()));
+
+        private async Task ExecuteBotaoVoltarTitleViewCommand() => await Shell.Current.GoToAsync("//paginaInicial");
     }
 }

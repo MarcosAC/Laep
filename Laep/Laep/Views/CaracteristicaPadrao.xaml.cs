7247fa16d5b333344ca6a3348c365328b73ba928
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-01-03 20:44:22 -0300

    Refatoração

---
+++ Laep/Laep/Views/CaracteristicaPadrao.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Laep.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CaracteristicaPadrao : ContentPage
+	{
+		public CaracteristicaPadrao ()
+		{
+			InitializeComponent ();
+		}
+	}
+}

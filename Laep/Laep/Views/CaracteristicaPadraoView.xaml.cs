a04893f7fd8c51528e677d8c8ab7fa90f6c30fd6
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-01-13 21:37:19 -0300

    Refatoração Pickers

--- Laep/Laep/Views/CaracteristicaPadraoView.xaml.cs
+++ Laep/Laep/Views/CaracteristicaPadraoView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
 
 namespace Laep.Views
 {
@@ -7,6 +8,34 @@ namespace Laep.Views
 		public CaracteristicaPadraoView ()
 		{
 			InitializeComponent ();
+
+            ListaModeloCaixa();
+            ListaDisjuntor();
 		}
-	}
+
+        private void ListaModeloCaixa()
+        {
+            var listaCaixa = new List<string> { "Monofasico", "Bifasico", "Trifasico" };
+
+            foreach (var item in listaCaixa)
+            {
+                pickerModeloCaixa1.Items.Add(item);
+                pickerModeloCaixa2.Items.Add(item);
+                pickerModeloCaixa3.Items.Add(item);
+            }
+        }
+
+        private void ListaDisjuntor()
+        {
+            var listaDisjuntor = new List<string> { "40A", "50A", "60A", "70A" };
+                                                   
+            foreach (var item in listaDisjuntor)       
+            {
+                pickerDisjuntor1.Items.Add(item);
+                pickerDisjuntor2.Items.Add(item);
+                pickerDisjuntor3.Items.Add(item);
+            }
+        }
+
+    }
 }

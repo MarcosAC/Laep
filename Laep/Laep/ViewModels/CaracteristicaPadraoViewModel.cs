520fe28f8dfb48e131e9e9cd86abd4c75fe14c65
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-01-20 12:49:12 -0300

    amperagem disjuntores de acordo com a escolha do tipo de caixa

--- Laep/Laep/ViewModels/CaracteristicaPadraoViewModel.cs
+++ Laep/Laep/ViewModels/CaracteristicaPadraoViewModel.cs
@@ -1,12 +1,49 @@
-﻿using System.Collections.ObjectModel;
+﻿using Prodfy.ViewModels;
+using System.Collections.Generic;
 
 namespace Laep.ViewModels
 {
-    public class CaracteristicaPadraoViewModel
-    {
-        public ObservableCollection<string> QuantidadeCaixas { get; } = new ObservableCollection<string> { "1", "2", "3" };
-        public ObservableCollection<string> Tensao { get; } = new ObservableCollection<string> { "Sistema Trifásico 127/220V", "Sistema Monofásico 120/240V" };
-        public ObservableCollection<string> ModeloCaixas { get; } = new ObservableCollection<string> { "Monofasico", "Bifasico", "Trifasico" };
-        public ObservableCollection<string> Disjuntores { get; } = new ObservableCollection<string> { "40A", "50A", "60A", "70A" };
+    public class CaracteristicaPadraoViewModel : BaseViewModel
+    { 
+        public List<string> ListaQuantidadeCaixas { get; } = new List<string> { "1", "2", "3" };
+        public List<string> ListaTensao { get; } = new List<string> { "Sistema Trifásico 127/220V", "Sistema Monofásico 120/240V" };
+        public List<string> ListaModeloCaixas { get; } = new List<string> { "Monofasico", "Bifasico", "Trifasico" };
+
+        readonly List<string> ListaDisjuntores = new List<string>();
+
+        private string _modeloCaixaSelecionado;
+        public string ModeloCaixaSelecionado
+        {
+            get => _modeloCaixaSelecionado;
+            set
+            {
+                SetProperty(ref _modeloCaixaSelecionado, value);
+                ListaDisjuntores.Clear();
+
+                if (Disjuntores != null)
+                    Disjuntores = null;
+
+                if (_modeloCaixaSelecionado == "Monofasico")
+                {
+                    ListaDisjuntores.Add("40A");
+                    ListaDisjuntores.Add("50A");
+                    ListaDisjuntores.Add("70A");
+                    Disjuntores = ListaDisjuntores;
+                }
+                else
+                {
+                    ListaDisjuntores.Add("40A");
+                    ListaDisjuntores.Add("60A");
+                    Disjuntores = ListaDisjuntores;
+                }
+            }
+        }
+
+        private List<string> _disjuntores;
+        public List<string> Disjuntores
+        {
+            get => _disjuntores;
+            set => SetProperty(ref _disjuntores, value);
+        }
     }
 }

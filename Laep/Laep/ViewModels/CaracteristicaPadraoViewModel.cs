9b4df69c2261d86ae6fad48293dbcad5c0fe3908
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-01-23 11:38:35 -0300

    Fix pickers amperes disjuntores

--- Laep/Laep/ViewModels/CaracteristicaPadraoViewModel.cs
+++ Laep/Laep/ViewModels/CaracteristicaPadraoViewModel.cs
@@ -11,43 +11,142 @@ namespace Laep.ViewModels
 
         readonly List<string> ListaDisjuntores = new List<string>();
 
-        private string _modeloCaixaSelecionado;
-        public string ModeloCaixaSelecionado
+        private string _modeloCaixaSelecionado1;
+        public string ModeloCaixaSelecionado1
         {
-            get => _modeloCaixaSelecionado;
+            get => _modeloCaixaSelecionado1;
             set
             {
-                SetProperty(ref _modeloCaixaSelecionado, value);
-                AmperagemDisjuntores(_modeloCaixaSelecionado);
+                SetProperty(ref _modeloCaixaSelecionado1, value);
+                AmperagemDisjuntores(_modeloCaixaSelecionado1, "disjuntor1");
             }
         }
 
-        private List<string> _disjuntores;
-        public List<string> Disjuntores
+        private string _modeloCaixaSelecionado2;
+        public string ModeloCaixaSelecionado2
         {
-            get => _disjuntores;
-            set => SetProperty(ref _disjuntores, value);
+            get => _modeloCaixaSelecionado2;
+            set
+            {
+                SetProperty(ref _modeloCaixaSelecionado2, value);
+                AmperagemDisjuntores(_modeloCaixaSelecionado2, "disjuntor2");
+            }
         }
 
-        private void AmperagemDisjuntores(string tipoCaixa)
+        private string _modeloCaixaSelecionado3;
+        public string ModeloCaixaSelecionado3
         {
-            ListaDisjuntores.Clear();
+            get => _modeloCaixaSelecionado3;
+            set
+            {
+                SetProperty(ref _modeloCaixaSelecionado3, value);
+                AmperagemDisjuntores(_modeloCaixaSelecionado3, "disjuntor3");
+            }
+        }
 
-            if (Disjuntores != null)
-                Disjuntores = null;
+        private List<string> _disjuntores1;
+        public List<string> Disjuntores1
+        {
+            get => _disjuntores1;
+            set => SetProperty(ref _disjuntores1, value);
+        }
 
+        private List<string> _disjuntores2;
+        public List<string> Disjuntores2
+        {
+            get => _disjuntores2;
+            set => SetProperty(ref _disjuntores2, value);
+        }
+
+        private List<string> _disjuntores3;
+        public List<string> Disjuntores3
+        {
+            get => _disjuntores3;
+            set => SetProperty(ref _disjuntores3, value);
+        }
+
+        private void AmperagemDisjuntores(string tipoCaixa, string disjuntor)
+        {
             if (tipoCaixa == "Monofasico")
             {
-                ListaDisjuntores.Add("40A");
-                ListaDisjuntores.Add("50A");
-                ListaDisjuntores.Add("70A");
-                Disjuntores = ListaDisjuntores;
+                if (ListaDisjuntores != null)
+                {
+                    ListaDisjuntores.Clear();
+
+                    ListaDisjuntores.Add("40A");
+                    ListaDisjuntores.Add("50A");
+                    ListaDisjuntores.Add("70A");
+                }
+
+                if (disjuntor == "disjuntor1")
+                {
+                    if (Disjuntores1 != null)
+                    {
+                        Disjuntores1 = null;
+                    }
+
+                    Disjuntores1 = ListaDisjuntores;
+                }                    
+
+                if (disjuntor == "disjuntor2")
+                {
+                    if (Disjuntores2 != null)
+                    {
+                        Disjuntores2 = null;
+                    }
+
+                    Disjuntores2 = ListaDisjuntores;
+                }                    
+
+                if (disjuntor == "disjuntor3")
+                {
+                    if (Disjuntores3 != null)
+                    {
+                        Disjuntores3 = null;
+                    }
+
+                    Disjuntores3 = ListaDisjuntores;
+                }
             }
             else
             {
-                ListaDisjuntores.Add("40A");
-                ListaDisjuntores.Add("60A");
-                Disjuntores = ListaDisjuntores;
+                if (ListaDisjuntores != null)
+                {
+                    ListaDisjuntores.Clear();
+
+                    ListaDisjuntores.Add("40A");
+                    ListaDisjuntores.Add("60A");
+                }
+
+                if (disjuntor == "disjuntor1")
+                {
+                    if (Disjuntores1 != null)
+                    {
+                        Disjuntores1 = null;
+                    }
+
+                    Disjuntores1 = ListaDisjuntores;
+                }
+
+                if (disjuntor == "disjuntor2")
+                {
+                    if (Disjuntores2 != null)
+                    {
+                        Disjuntores2 = null;
+                    }
+
+                    Disjuntores2 = ListaDisjuntores;
+                }
+
+                if (disjuntor == "disjuntor3")
+                {
+                    if (Disjuntores3 != null)
+                    {
+                        Disjuntores3 = null;
+                    }
+
+                   Disjuntores3 = ListaDisjuntores;
+                }
             }
         }
     }

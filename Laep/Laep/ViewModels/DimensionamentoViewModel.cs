35615c216fe0497c99214d4ac8efcebbbe45f48e
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-01-24 15:20:10 -0300

    Botão voltar titleview ok

---
+++ Laep/Laep/ViewModels/DimensionamentoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Laep.ViewModels
+{
+    public class DimensionamentoViewModel : BaseViewModel
+    {
+        private Command _botaoVoltarTitleViewCommand;
+        public Command BotaoVoltarTitleViewCommand =>
+            _botaoVoltarTitleViewCommand ?? (_botaoVoltarTitleViewCommand = new Command(async () => await ExecuteBotaoVoltarTitleViewCommand()));
+
+        private async Task ExecuteBotaoVoltarTitleViewCommand() => await Shell.Current.GoToAsync("//caracteriscaPadrao");
+    }
+}

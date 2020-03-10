85a61019e20779a3bdc27677d8cfb6c0a6a42603
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-03-10 11:15:30 -0300

    refatoracao

---
+++ Laep/Laep/Models/Dimensionamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laep.Models
+{
+    public class Dimensionamento
+    {       
+        public string ValorMultiplex { get; set; }
+        public string ValorEntrada { get; set; }
+        public string ValorFases { get; set; }
+        public string ValorNeutro { get; set; }
+        public string Protecao { get; set; }
+        public string EletrodutoPcv { get; set; }
+        public string EletrodutoAco { get; set; }
+        public string NumeroDeEletrodos { get; set; }
+        public string CondutorDeAterramento { get; set; }
+        public string Tensao{ get; set; }
+    }
+}
---
+++ Laep/Laep/Utils/GerarDimensionamento.cs
@@ -0,0 +1,15 @@
+﻿using Laep.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laep.Utils
+{
+    public class GerarDimensionamento
+    {
+        public void Dimensionar(Dimensionamento valoresDimensionamento)
+        {
+
+        }
+    }
+}

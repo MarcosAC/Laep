4ea1dabc626b6b58a282cb87ced89fcbd7c511fb
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-03-16 10:50:38 -0300

    Adição da classe ResultadoDimensionamento

--- Laep/Laep/Models/ResultadoDimensionamento.cs
+++ Laep/Laep/Models/ResultadoDimensionamento.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Laep.Models
+﻿namespace Laep.Models
 {
-    class ResultadoDimensionamento
+    public class ResultadoDimensionamento
     {
+        public string valorMultiplex { get; set; }
+        public string valorEntrada { get; set; }
+        public string valorFases { get; set; }
+        public string valorNeutro { get; set; }
+        public string protecao { get; set; }
+        public string eletrodutoPcv { get; set; }
+        public string eletrodutoAco { get; set; }
+        public string numeroDeEletrodos { get; set; }
+        public string condutorDeAterramento { get; set; }
     }
 }
